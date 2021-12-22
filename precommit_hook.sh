04819cf1f413804e198cf3ea293c5d430d758421
Ingmar <Ingmarvdg@gmail.com>
2021-12-22 14:12:25 +0100

    add pre-commit hooks

---
+++ precommit_hook.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+check_file() {
+    local file=$1
+    local match_pattern=$2
+
+    local file_changes_with_context=$(git diff -U999999999 -p --cached --color=always -- $file)
+
+    # From the diff, get the green lines starting with '+' and including '$match_pattern'
+    local matched_additions=$(echo "$file_changes_with_context" | grep -C4 $'^\e\\[32m\+.*'"$match_pattern")
+
+    if [ -n "$matched_additions" ]; then
+        echo -e "\n$file additions match '$match_pattern':\n"
+
+        for matched_line in $matched_additions
+        do
+            echo "$matched_line"
+        done
+        echo "Not committing, because $file contains a $match_pattern"
+        exit 1
+
+    fi
+}
+
+# Actual hook logic:
+
+MATCH=$(git config --get-all hooks.confirm.match)
+if [ -z "$MATCH" ]; then
+    echo "Git-Confirm: hooks.confirm.match not set, defaulting to 'TODO'"
+    echo 'Add matches with `git config --add hooks.confirm.match "string-to-match"`'
+    MATCH='TODO'
+fi
+
+for file in `git diff --cached -p --name-status | cut -c3-`; do
+    for match_pattern in $MATCH
+    do
+        check_file $file $match_pattern
+    done
+done
+exit
